d2bffbfffd931109adc028ce8641bfb08b7bb00e
jamesonamadeus <jamesonamadeus@gmail.com>
2015-07-22 21:03:40 -0400

    Yessssss!!!!

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -37,3 +37,23 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
      });
 
 });
+
+$("#outer-wrap a.btn[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top -100
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
