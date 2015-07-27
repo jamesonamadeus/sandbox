cfc1fd2c7c2d5571a5b49c4690c4413b5de4ebae
jamesonamadeus <jamesonamadeus@gmail.com>
2015-07-26 23:33:25 -0400

    Javascript

--- projects/javascript/intro/scripts.js
+++ projects/javascript/intro/scripts.js
@@ -1,7 +1,11 @@
-function add(num1,num2){
-	sum = num1 + num2;
-	return sum;
-
+x = 4;
+switch(x){
+	case 2:
+		document.write("x is 2");
+		break;
+	case 5:
+		document.write("x is 5");
+		break;
+	default:
+		document.write("x is");
 }
-
-document.write(add("5","5"));
