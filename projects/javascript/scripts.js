13c608bbdd4235b0632b72b3e40d80038472e05c
jamesonamadeus <jamesonamadeus@gmail.com>
2015-07-26 21:02:26 -0400

    javascript etc.

--- projects/javascript/scripts.js
+++ projects/javascript/scripts.js
@@ -10,3 +10,10 @@ num = "Hello ";
 txt = 100;
 
 document.write("<br>" + num +txt);
+
+/*This Function greets the user with the name entered*/
+function greetUser() {
+	var userInput = document.getElementById('userName').value; //TODO: clarify this
+	document.getElementById('greeting').innerHTML = 'Hello,' + userInput;
+}
+
