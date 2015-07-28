5961b513b33ee02b43bd90587318a18b7f200133
jamesonamadeus <jamesonamadeus@gmail.com>
2015-07-27 22:59:16 -0400

    Else If

--- projects/javascript/firstscript/main.js
+++ projects/javascript/firstscript/main.js
@@ -1,2 +1,10 @@
-var myName = "Chris";
+var myName = "Sean";
+
+if(myName === "John") {
+	console.log("How about that?");
+} else if(myName === "Chris") {
+	console.log("Hello, Chris!");
+} else {
+	console.log("Your name is John.")
+}
 console.log(myName);
