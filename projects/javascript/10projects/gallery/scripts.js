93e195196187f405c9383616cc69f9e5e5d0c823
jamesonamadeus <jamesonamadeus@gmail.com>
2015-08-12 23:11:52 -0400

    image gallery

--- projects/javascript/10projects/gallery/scripts.js
+++ projects/javascript/10projects/gallery/scripts.js
@@ -1,10 +1,20 @@
 function myfunction() {
-	var a = document.getElementsByTagName("img")[0]
-	var atts = new Array()
-	atts["alt"] = a.getAttribute("alt");
-	atts["title"] = a.getAttribute("title")
-	atts["src"] = a.getAttribute("src")
-	alert(atts["src"])
+// 	var a = document.getElementsByTagName("img")[0]
+// 	var atts = new Array()
+// 	atts["alt"] = a.getAttribute("alt");
+// 	atts["title"] = a.getAttribute("title")
+// 	atts["src"] = a.getAttribute("src")
+// 	alert(atts["src"])
+// }
 
+// window.onload = function(){
+// 	var img = document.getElementById("theimage")
+// 	var src = img.getAttribute("src")
+// 	var length = src.length - 4
+// 	src = src.substring(0,length)
+// 	img.setAttribute("alt", src)
+
+	var img = document.getElementById("theimage")
+	img.setAttribute("src","umbrella.jpg")
 
 }
