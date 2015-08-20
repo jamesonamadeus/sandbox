f3b1220810f077c99471498cc361d3833f304aba
jamesonamadeus <jamesonamadeus@gmail.com>
2015-08-19 22:42:44 -0400

    Image Gallery cont.

--- projects/javascript/10projects/gallery/scripts.js
+++ projects/javascript/10projects/gallery/scripts.js
@@ -1,8 +1,8 @@
-	var X
-	var randomvariable = X
+	// var X
+	// var randomvariable = X
 
 
-function myfunction() {
+// function myfunction() {
 // 	var a = document.getElementsByTagName("img")[0]
 // 	var atts = new Array()
 // 	atts["alt"] = a.getAttribute("alt");
@@ -18,20 +18,31 @@ function myfunction() {
 // 	src = src.substring(0,length)
 // 	img.setAttribute("alt", src)
 
-	var img = document.getElementById("theimage")
-	var thesrc = img.getAttribute("src")
-	var length = thesrc.length - 9
-	thesrc = thesrc.substring(length, length + 5)
-if (thesrc == "thumb"){
-	var oldsrc = img.getAttribute("src")
-	img.setAttribute('src',oldsrc.substring(0, length) + "full.jpg")
+// 	var img = document.getElementById("theimage")
+// 	var thesrc = img.getAttribute("src")
+// 	var length = thesrc.length - 9
+// 	thesrc = thesrc.substring(length, length + 5)
+// if (thesrc == "thumb"){
+// 	var oldsrc = img.getAttribute("src")
+// 	img.setAttribute('src',oldsrc.substring(0, length) + "full.jpg")
 
-	// img.setAttribute('src',"image-full.jpg")
+// 	// img.setAttribute('src',"image-full.jpg")
 
-} else {
-	var oldsrc = img.getAttribute("src")
-	img.setAttribute('src',oldsrc.substring(0, length + 1) + "image-thumb.jpg")
+// } else {
+// 	var oldsrc = img.getAttribute("src")
+// 	img.setAttribute('src',oldsrc.substring(0, length + 1) + "image-thumb.jpg")
 
-	// img.setAttribute('src',"image-thumb.jpg")
-}
-}
+// 	// img.setAttribute('src',"image-thumb.jpg")
+// }
+// }
+
+var newsrc = document.getElementsByClassName('switchclass')
+	for (var i = 0; i < newsrc.length; i++){
+		var thenewsrc = newsrc [i];
+		thenewsrc.onclick = function(){
+			var thesrc = this.getAttribute("src")
+			document.getElementsByClassName('thefeaturedimg')[0].setAttribute("src", thesrc)
+		}
+
+	}
+
